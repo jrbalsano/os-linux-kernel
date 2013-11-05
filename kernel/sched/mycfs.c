99725f7bed81ae1194514990af39f89a47a30650
Daron <daronlin303@gmail.com>
2013-11-05 17:07:09 -0500

    Kind of implemented dequeue_task_mycfs

--- kernel/sched/mycfs.c
+++ kernel/sched/mycfs.c
@@ -5,7 +5,22 @@ enqueue_task_mycfs(struct rq *rq, struct task_struct *p, int flags)
 	struct sched_mycfs_entity *mycfs = &p->mycfs;
 	
 	if (mycfs && !mycfs->on_rq) {
-		cfs_rq = &rq->my_cfs
+		mycfs_rq = &rq->my_cfs
 		enqueue_entity(mycfs_rq, mycfs, flags);
 	}
 }
+
+
+static void dequeue_task_mycfs(struct rq *rq, struct task_struct *p, int flags)
+{
+
+	struct mycfs_rq *mycfs_rq;
+	struct sched_entity *mycfs = &p->mycfs;
+
+	if(mycfs && mycfs->on_rq){
+		mycfs_rq = &rq->my_cfs;
+		dequeue_entity(mycfs_rq, mycfs, flags);
+	}
+}
+
+
