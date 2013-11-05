50bbaa305cce4a5427dd35c1a36f8606ceba6378
Jonathan Balsano <jrb2194@columbia.edu>
2013-11-05 16:52:48 -0500

    Kind of completed enqueue

--- kernel/sched/mycfs.c
+++ kernel/sched/mycfs.c
@@ -1,7 +1,11 @@
-
 static void
 enqueue_task_mycfs(struct rq *rq, struct task_struct *p, int flags)
 {
-	struct mycfs_rq *cfs_rq;
+	struct mycfs_rq *mycfs_rq;
 	struct sched_mycfs_entity *mycfs = &p->mycfs;
+	
+	if (mycfs && !mycfs->on_rq) {
+		cfs_rq = &rq->my_cfs
+		enqueue_entity(mycfs_rq, mycfs, flags);
+	}
 }
