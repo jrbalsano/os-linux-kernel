04101e87a461c817fa36b49a1a5e883852d6fbeb
Daron <daronlin303@gmail.com>
2013-11-19 18:37:42 -0500

    hotfix - defined user_struct wrong

--- arch/arm/kernel/set_mlimit.c
+++ arch/arm/kernel/set_mlimit.c
@@ -3,7 +3,7 @@
 
 asmlinkage int set_mlimit(uid_t uid, long mem_max){
 
-   user_struct *user = find_user(uid);
+   struct user_struct *user = find_user(uid);
    
    if(user && mem_max > -1){
    	user->mem_max = mem_max;
